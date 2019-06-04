a1795a2a742e25b839201578542e411fe868a418
LukePernigotti <luke.pernigotti.90@gmail.com>
2019-06-04 19:41:03 -0300

    Updates

---
+++ precache-manifest.a8087229be97181d5a4dbaa97ce118ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18abcee58b676482177915770745ab1a",
+    "url": "/WeatherAPI-ReactJS/index.html"
+  },
+  {
+    "revision": "7e29da7b724515a91201",
+    "url": "/WeatherAPI-ReactJS/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f337598c660243129c6f",
+    "url": "/WeatherAPI-ReactJS/static/css/main.c82877c4.chunk.css"
+  },
+  {
+    "revision": "7e29da7b724515a91201",
+    "url": "/WeatherAPI-ReactJS/static/js/2.4aeac1f2.chunk.js"
+  },
+  {
+    "revision": "f337598c660243129c6f",
+    "url": "/WeatherAPI-ReactJS/static/js/main.6f60ce55.chunk.js"
+  },
+  {
+    "revision": "e38d089bd00353d6f1cb",
+    "url": "/WeatherAPI-ReactJS/static/js/runtime~main.72786602.js"
+  }
+]);
